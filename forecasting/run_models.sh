e988b7a2b7546ae541dec042ec765b345049ee88
vilhess <samy.vilhes@gmail.com>
2025-04-28 15:39:04 +0200

    fresh

--- forecasting/run_models.sh
+++ forecasting/run_models.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Define datasets to iterate over
-datasets=( "etth1" "etth2" "ettm1" "ettm2"  "exchange_rate" "weather" "national_illness") # electricity 
+datasets=(  "etth2" "ettm1" "ettm2"  "exchange_rate" "weather" "national_illness") # electricity "etth1"
 
 for DATASET in "${datasets[@]}"; do
     echo "Running experiments on dataset: $DATASET"
-#    python main.py forecasting=$DATASET encoder=config_encoder_small forecasting.univariate=False
-#    python main.py forecasting=$DATASET encoder=config_encoder_small forecasting.univariate=True
-    python main.py forecasting=$DATASET encoder=config_encoder_base forecasting.univariate=False forecasting.revin=True
-    python main.py forecasting=$DATASET encoder=config_encoder_base forecasting.univariate=True forecasting.revin=True
+    python main.py forecasting=$DATASET encoder=config_encoder_big forecasting.univariate=False
+    python main.py forecasting=$DATASET encoder=config_encoder_big forecasting.univariate=True
+    python main.py forecasting=$DATASET encoder=config_encoder_big forecasting.univariate=False forecasting.revin=True
+    python main.py forecasting=$DATASET encoder=config_encoder_big forecasting.univariate=True forecasting.revin=True
 done
