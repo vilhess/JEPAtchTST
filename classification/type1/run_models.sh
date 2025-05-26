9a0013042ab6e4d42f59f17319f189f3bdd5ae1a
vilhess <samy.vilhes@gmail.com>
2025-05-26 15:17:19 +0200

    dir

--- classification/type1/run_models.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-sizes=("small" "base" "big") # 
-
-for SIZE in "${sizes[@]}"; do
-    echo "Running experiments on size: $SIZE"
-    python main.py encoder=config_encoder_$SIZE
-    done
-done
