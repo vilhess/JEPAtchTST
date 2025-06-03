f84990cadcbf731ededd73fb539d9987754bd9f3
vilhess <samy.vilhes@gmail.com>
2025-06-03 08:06:38 +0200

    fresh

--- forecasting/run_models.sh
+++ forecasting/run_models.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
 # Define datasets to iterate over
-datasets=("etth1" "etth2" "ettm1" "ettm2" "exchange_rate" "weather" "national_illness" ) # electricity 
+datasets=("etth1" "etth2" "ettm1" "ettm2" "exchange_rate" "weather" "national_illness" )
 
 for DATASET in "${datasets[@]}"; do
     echo "Running experiments on dataset: $DATASET"
-    python main.py forecasting=$DATASET forecasting.univariate=False
-    python main.py forecasting=$DATASET forecasting.univariate=True
     python main.py forecasting=$DATASET forecasting.univariate=False forecasting.revin=True
     python main.py forecasting=$DATASET forecasting.univariate=True forecasting.revin=True
 done
